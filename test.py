8107976b2147af716b327c18aabc88b6f5268cb5
sus2790 <104747821+sus2790@users.noreply.github.com>
2022-11-09 21:43:14 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,7 @@
 import aiohttp
 from typing import Optional, Union, List
-
-
 class aa:
-    def __init__(
-        self,
-    ) -> None:
+ def __init__(self,
+ ) -> None:
         ...
-
-    async def send(self, content: str):
-        return
+  async def send(self, content: str):return
